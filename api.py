cdc797927f7dc66468440135d51e1bfcf3a7320a
Fernando Henrique <fhenriquesilva@outlook.com>
2024-04-04 18:43:31 -0300

    Ok

--- api.py
+++ api.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
-
+from prometheus.middleware import middleware #Adicionado para monitoramento Prometheus
+from prometheus.metrics import REQUEST_COUNT, PROCESSING_TIME #Adicionado para monitoramento Prometheus
 
 app_name = 'comentarios'
 app = Flask(app_name)
@@ -10,31 +11,40 @@ app.debug = True
 comments = {}
 
 
+app.before_request(middleware)
+
 @app.route('/api/comment/new', methods=['POST'])
 def api_comment_new():
-    request_data = request.get_json()
+    with PROCESSING_TIME.time():
+        request_data = request.get_json()
 
-    email = request_data['email']
-    comment = request_data['comment']
-    content_id = '{}'.format(request_data['content_id'])
+                                 
+                                     
+                                                        
 
-    new_comment = {
-            'email': email,
-            'comment': comment,
-            }
+        email = request_data['email']
+        comment = request_data['comment']
+        content_id = '{}'.format(request_data['content_id'])
+             
 
-    if content_id in comments:
-        comments[content_id].append(new_comment)
-    else:
-        comments[content_id] = [new_comment]
+        new_comment = {
+                'email': email,
+                'comment': comment,
+                }
 
-    message = 'comment created and associated with content_id {}'.format(content_id)
-    response = {
-            'status': 'SUCCESS',
-            'message': message,
-            }
-    return jsonify(response)
+        if content_id in comments:
+            comments[content_id].append(new_comment)
+        else:
+            comments[content_id] = [new_comment]
+             
+                            
 
+        message = 'comment created and associated with content_id {}'.format(content_id)
+        response = {
+                'status': 'SUCCESS',
+                'message': message,
+                }
+        return jsonify(response)
 
 @app.route('/api/comment/list/<content_id>')
 def api_comment_list(content_id):
@@ -49,3 +59,6 @@ def api_comment_list(content_id):
                 'message': message,
                 }
         return jsonify(response), 404
+
+if __name__ == '__main__':
+    app.run()
---
+++ prometheus/middleware.py
@@ -0,0 +1,3 @@
+from prometheus_client import HTTPRequestDurationMiddleware
+
+middleware = HTTPRequestDurationMiddleware()
